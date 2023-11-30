d400fc78531d78244b931ef6634023c20f764bc1
Zlendy <zlendy@proton.me>
2023-11-30 23:24:49 +0100

    feat: Read incoming data by lines and set nonblocking TCP Listener

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
 use std::{
-    io::Read,
+    io::{self, BufRead, BufReader},
     net::{TcpListener, TcpStream},
 };
 
-fn handle_client(mut stream: TcpStream) {
-    let mut string = String::new();
-    let payload = stream.read_to_string(&mut string);
-
-    match payload {
-        Ok(_) => {
-            println!("Incoming: {}", string);
-        }
-        Err(data) => {
-            println!("ERROR: {}", data)
+fn handle_client(stream: TcpStream) {
+    let stream = BufReader::new(stream);
+    for payload in stream.lines() {
+        match payload {
+            Ok(data) => {
+                println!("Incoming: {}", data);
+            }
+            Err(data) => {
+                println!("ERROR: {}", data)
+            }
         }
     }
 }
@@ -20,11 +20,27 @@ fn handle_client(mut stream: TcpStream) {
 fn main() -> std::io::Result<()> {
     let address = "127.0.0.1:8080";
     let listener = TcpListener::bind(address)?;
+    listener
+        .set_nonblocking(true)
+        .expect("Cannot set non-blocking");
     println!("Server listening on {}", address);
 
     // accept connections and process them serially
     for stream in listener.incoming() {
-        handle_client(stream?);
+        match stream {
+            Ok(s) => {
+                // do something with the TcpStream
+                handle_client(s);
+            }
+            Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
+                // wait until network socket is ready, typically implemented
+                // via platform-specific APIs such as epoll or IOCP
+                // wait_for_fd();
+                continue;
+            }
+            Err(e) => panic!("encountered IO error: {e}"),
+        }
     }
+
     Ok(())
 }
