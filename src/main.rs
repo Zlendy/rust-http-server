9ac82d9fd64d0a6dfe33907f6ae3c826d8121321
Zlendy <zlendy@proton.me>
2023-12-03 12:23:10 +0100

    refactor: Move handle_client() and write_response() into stream.rs

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,7 @@
 mod modules;
 
-use std::{
-    io::{self, BufRead, BufReader, Write},
-    net::{TcpListener, TcpStream},
-};
-
-use crate::modules::{
-    file::{get_mime, read_file_vec},
-    http::{http_200, HTTP_404, HTTP_405},
-    vec::{slice_to_bytes_vec, slice_to_vec},
-};
-
-fn write_response(stream: &mut TcpStream, payload: Vec<String>) {
-    match stream.write(slice_to_bytes_vec(payload).as_slice()) {
-        Ok(_) => {}
-        Err(data) => {
-            println!("{:#?}", data);
-        }
-    }
-}
-
-fn handle_client(mut stream: TcpStream) -> io::Result<()> {
-    let reader = BufReader::new(stream.try_clone()?);
-
-    let http_request: Vec<String> = reader
-        .lines()
-        .map(|result| result.unwrap_or_default())
-        .take_while(|line| !line.is_empty())
-        .collect();
-
-    println!("Request: {:#?}", http_request);
-
-    let line1: Vec<&str> = http_request[0].split(" ").into_iter().collect();
-    let (http_method, http_path, _http_version) = (line1[0], line1[1], line1[2]);
-    if http_method != "GET" {
-        write_response(&mut stream, slice_to_vec(&HTTP_405));
-        return Ok(());
-    }
-
-    match read_file_vec(format!("./public{}", http_path).as_str()) {
-        Ok(data) => {
-            let payload = http_200(
-                get_mime(http_path.split(".").last().unwrap_or_default()),
-                vec![data.lines().collect()],
-            );
-            write_response(&mut stream, payload);
-        }
-        Err(_) => {
-            write_response(&mut stream, slice_to_vec(&HTTP_404));
-        }
-    }
-
-    Ok(())
-}
+use crate::modules::stream::handle_client;
+use std::{io, net::TcpListener};
 
 fn main() -> std::io::Result<()> {
     let address = "0.0.0.0:8080";
--- src/modules/mod.rs
+++ src/modules/mod.rs
@@ -1,3 +1,4 @@
 pub mod file;
 pub mod http;
+pub mod stream;
 pub mod vec;
